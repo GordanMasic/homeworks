71cc313f7bfc0ef00035bffbb2a79ee413c82d82
GordanMasic <gordan.masic@bootcamp.ba>
2015-06-20 19:52:41 +0200

    Homework from Benjamin Talic

---
+++ homeworkLectures2/src/ba/bitcamp/homeworkLectures2/task06/Main.java
@@ -0,0 +1,14 @@
+package ba.bitcamp.homeworkLectures2.task06;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dog d = new Dog("Flafi", 7, "Dog", "Retriver");
+
+		System.out.println(d);
+
+		Cat c = new Cat("Mici", 5, "Cat");
+
+		System.out.println(c);
+	}
+}
